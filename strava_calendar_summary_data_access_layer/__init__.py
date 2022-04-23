bc4c458552f820aa18e0bacbdc6a6c27fe51a761
SebTota <kidokyo1@gmail.com>
2022-04-23 14:42:18 -0700

    Updated package init file

--- strava_calendar_summary_data_access_layer/__init__.py
+++ strava_calendar_summary_data_access_layer/__init__.py
@@ -1,4 +1,7 @@
 from __future__ import absolute_import
 
-from .models import StravaAuth, User, CalendarAuth
-from .controllers import UserController
+from .models.calendar_auth import CalendarAuth
+from .models.strava_auth import StravaAuth
+from .models.user import User
+
+from .controllers.user_controller import UserController
