1e4f8933594586e74b37df97824bfb169ce821ed
SebTota <kidokyo1@gmail.com>
2022-05-04 12:09:57 -0700

    bug fix: added title and description template to calendar event

--- strava_calendar_summary_data_access_layer/models/calendar/calendar_event.py
+++ strava_calendar_summary_data_access_layer/models/calendar/calendar_event.py
@@ -1,8 +1,9 @@
 class CalendarEvent:
-    def __init__(self, strava_event_id: str, calendar_event_id: str, calendar_template: str):
+    def __init__(self, strava_event_id: str, calendar_event_id: str, title_template: str, description_template: str):
         self.strava_event_id = strava_event_id
         self.calendar_event_id = calendar_event_id
-        self.calendar_template = calendar_template
+        self.title_template = title_template
+        self.description_template = description_template
 
     @staticmethod
     def from_dict(source):
