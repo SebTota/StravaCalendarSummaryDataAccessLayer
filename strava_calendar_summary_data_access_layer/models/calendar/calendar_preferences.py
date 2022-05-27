d3225926a91eebe211b3e0d803b8fec9185b335b
SebTota <kidokyo1@gmail.com>
2022-05-26 20:22:05 -0700

    Updated default title and summary template for aggregate summary calendar events

--- strava_calendar_summary_data_access_layer/models/calendar/calendar_preferences.py
+++ strava_calendar_summary_data_access_layer/models/calendar/calendar_preferences.py
@@ -2,13 +2,25 @@ from .end_of_week_type import EndOfWeekType
 
 DEFAULT_END_OF_WEEK = EndOfWeekType.SUNDAY
 DEFAULT_TITLE_TEMPLATE = '{distance_miles} {type}'
-DEFAULT_SUMMARY_TEMPLATE = 'Distance: {distance_miles}\nDuration: {duration}'
+DEFAULT_EVENT_TEMPLATE = 'Name: {name}\n' \
+                         'Distance: {distance_miles}\n' \
+                         'Duration: {duration}\n' \
+                         'Pace: {pace_min_per_mile}'
+DEFAULT_SUMMARY_TITLE_TEMPLATE = 'Total Distance: {distance_miles}'
+DEFAULT_SUMMARY_TEMPLATE = 'Total Distance: {distance_miles}\n' \
+                           'Total Duration: {duration}\n' \
+                           'Total Elevation Gain: {elevation_gain_feet}\n' \
+                           'Average Pace: {avg_pace_min_per_mile}'
 
 
 class CalendarPreferences:
-    def __init__(self, per_run_summary_enabled: bool = True, per_run_title_template: str = DEFAULT_TITLE_TEMPLATE, per_run_summary_template: str = DEFAULT_SUMMARY_TEMPLATE,
-                 daily_run_summary_enabled: bool = True, daily_run_title_template: str = DEFAULT_TITLE_TEMPLATE, daily_run_summary_template: str = DEFAULT_SUMMARY_TEMPLATE,
-                 weekly_run_summary_enabled: bool = True, weekly_run_title_template: str = DEFAULT_TITLE_TEMPLATE, weekly_run_summary_template: str = DEFAULT_SUMMARY_TEMPLATE,
+    def __init__(self, per_run_summary_enabled: bool = True, per_run_title_template: str = DEFAULT_TITLE_TEMPLATE,
+                 per_run_summary_template: str = DEFAULT_EVENT_TEMPLATE,
+                 daily_run_summary_enabled: bool = True, daily_run_title_template: str = DEFAULT_SUMMARY_TITLE_TEMPLATE,
+                 daily_run_summary_template: str = DEFAULT_SUMMARY_TEMPLATE,
+                 weekly_run_summary_enabled: bool = True,
+                 weekly_run_title_template: str = DEFAULT_SUMMARY_TITLE_TEMPLATE,
+                 weekly_run_summary_template: str = DEFAULT_SUMMARY_TEMPLATE,
                  end_of_week: EndOfWeekType = DEFAULT_END_OF_WEEK):
         self.per_run_summary_enabled = per_run_summary_enabled
         self.per_run_summary_template = per_run_summary_template
