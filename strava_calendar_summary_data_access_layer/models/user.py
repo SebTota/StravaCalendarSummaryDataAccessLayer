95f56ff9dae0e5732cde3f03292b4055e5fd41e5
SebTota <kidokyo1@gmail.com>
2022-05-29 22:28:01 -0700

    added daily and weekly calendar summary objects to user object

--- strava_calendar_summary_data_access_layer/models/user.py
+++ strava_calendar_summary_data_access_layer/models/user.py
@@ -1,5 +1,5 @@
 from .strava import StravaCredentials
-from .calendar import CalendarPreferences
+from .calendar import CalendarPreferences, CalendarSummaryEvent
 
 from google.oauth2.credentials import Credentials
 import json
@@ -10,7 +10,9 @@ class User:
                  strava_credentials: StravaCredentials = None,
                  calendar_credentials: Credentials = None,
                  calendar_id: str = None,
-                 calendar_preferences: CalendarPreferences = CalendarPreferences()):
+                 calendar_preferences: CalendarPreferences = CalendarPreferences(),
+                 daily_summary_calendar_event: CalendarSummaryEvent = None,
+                 weekly_summary_calendar_event: CalendarSummaryEvent = None):
         self.user_id: str = user_id
         self.first_name: str = first_name
         self.last_name: str = last_name
@@ -18,21 +20,35 @@ class User:
         self.calendar_credentials: Credentials = calendar_credentials
         self.calendar_id: str = calendar_id  # The id of the calendar belonging to the app if one is already created
         self.calendar_preferences: CalendarPreferences = calendar_preferences
+        self.daily_summary_calendar_event = daily_summary_calendar_event
+        self.weekly_summary_calendar_event = weekly_summary_calendar_event
 
     @staticmethod
     def from_dict(source):
         user_id = source['user_id']
         first_name = source['first_name']
         last_name = source['last_name']
+
         strava_credentials = StravaCredentials.from_dict(source['strava_credentials']) \
             if 'strava_credentials' in source else None
+
         calendar_credentials = Credentials.from_authorized_user_info(
             source['calendar_credentials']) if 'calendar_credentials' in source else None
+
         calendar_id = source['calendar_id'] if 'calendar_id' in source else None
+
         calendar_preferences = CalendarPreferences.from_dict(source['calendar_preferences']) \
             if 'calendar_preferences' in source else CalendarPreferences()
+
+        daily_summary_calendar_event = CalendarSummaryEvent.from_dict(source['daily_summary_calendar_event']) \
+            if 'daily_summary_calendar_event' in source else None
+
+        weekly_summary_calendar_event = CalendarSummaryEvent.from_dict(source['weekly_summary_calendar_event']) \
+            if 'weekly_summary_calendar_event' in source else None
+
         return User(user_id, first_name, last_name, strava_credentials,
-                    calendar_credentials, calendar_id, calendar_preferences)
+                    calendar_credentials, calendar_id, calendar_preferences, daily_summary_calendar_event,
+                    weekly_summary_calendar_event)
 
     def to_dict(self):
         d = {'user_id': self.user_id, 'first_name': self.first_name, 'last_name': self.last_name}
@@ -44,4 +60,8 @@ class User:
             d['calendar_id'] = self.calendar_id
         if self.calendar_preferences:
             d['calendar_preferences'] = self.calendar_preferences.to_dict()
+        if self.daily_summary_calendar_event:
+            d['daily_summary_calendar_event'] = self.daily_summary_calendar_event.to_dict()
+        if self.weekly_summary_calendar_event:
+            d['weekly_summary_calendar_event'] = self.weekly_summary_calendar_event.to_dict()
         return d
